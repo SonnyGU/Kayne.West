1d2e0261cf725d4fb40e6938697f179dc778d13d
Guson Ulysse <Gulysse@wgu.edu>
2023-11-22 22:32:40 -0500

    Adds a Rest API and button for Kanye

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
+    data = requests.get(url="https://api.kanye.rest/")
+    data.raise_for_status()
+
+    quote = data.json()["quote"]
+
+    canvas.itemconfig(quote_text, text=quote)
+
 
 
 
@@ -14,7 +20,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Click on Kayne for quotes!", width=250, font=("Arial", 20, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
